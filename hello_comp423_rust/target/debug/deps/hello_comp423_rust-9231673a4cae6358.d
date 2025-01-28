a91f601139a3bf2e3e437e81a3b98c7c39b1c473
Hamsini Tankala <htankala@ad.unc.edu>
2025-01-28 17:35:00 +0000

    finished rust tutorial

---
+++ hello_comp423_rust/target/debug/deps/hello_comp423_rust-9231673a4cae6358.d
@@ -0,0 +1,5 @@
+/workspaces/rust_tutorial/hello_comp423_rust/target/debug/deps/hello_comp423_rust-9231673a4cae6358: src/main.rs
+
+/workspaces/rust_tutorial/hello_comp423_rust/target/debug/deps/hello_comp423_rust-9231673a4cae6358.d: src/main.rs
+
+src/main.rs:
---
+++ hello_comp423_rust/target/debug/hello_comp423_rust.d
@@ -0,0 +1,1 @@
+/workspaces/rust_tutorial/hello_comp423_rust/target/debug/hello_comp423_rust: /workspaces/rust_tutorial/hello_comp423_rust/src/main.rs
